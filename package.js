119e03e191d81bb731cdcc7318c6059255ae1702
Anderson Arboleya <anderson@arboleya.me>
2015-04-16 02:51:53 -0300

    improving meteor descriptor

--- package.js
+++ package.js
@@ -1,5 +1,7 @@
+var name = 'arboleya:ways-addressbar';
+
 Package.describe({
-  name: 'arboleya:ways-addressbar',
+  name: name,
   version: '0.2.0',
   summary: 'AddressBar plugin for Ways, supporting pushState and #hash urls',
   git: 'https://github.com/arboleya/ways-addressbar',
@@ -7,7 +9,18 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
+  // api.use('arboleya:happens', 'client');
   api.versionsFrom('1.0');
-  api.addFiles('ways-browser.js', 'client');
-  api.export('ways_browser', 'client');
+  api.addFiles('lib/hash.js', 'client');
+  api.addFiles('lib/history.js', 'client');
+  api.addFiles('lib/index.js', 'client');
+  api.export('WaysAddressBar');
+  
+});
+
+Package.onTest(function (api) {
+  api.use(name);
+  api.use('tinytest');
+  api.use('arboleya:happens', 'client');
+  api.addFiles('test/tests/meteor.js');
 });
