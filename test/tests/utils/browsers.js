319dda5f8c0bf7cb86c6893a67ad115d251ca9ac
Anderson Arboleya <anderson@arboleya.me>
2015-04-17 00:14:54 -0300

    refactoring class and updating browsers' matrix

--- test/tests/utils/browsers.js
+++ test/tests/utils/browsers.js
@@ -3,10 +3,10 @@
  */
 exports['local'] = function() {
   return [
-    {browserName: 'phantomjs', tags: ['ways-addressbar', 'phantomjs', 'bal', 'ble']},
-    {browserName: 'chrome', tags: ['ways-addressbar', 'chrome']},
-    {browserName: 'firefox', tags: ['ways-addressbar', 'firefox']},
-    {browserName: 'safari', tags: ['ways-addressbar', 'safari']}
+    env(null, 'phantomjs'),
+    env(null, 'chrome'),
+    env(null, 'firefox'),
+    env(null, 'safari'),
   ];
 };
 
@@ -15,162 +15,98 @@ exports['local'] = function() {
  */
 exports['sauce labs'] = function() {
   return [
-
-    /**
-     * phantomjs
-     */
-    {
-      browserName: 'phantomjs', tags: ['ways-addressbar', 'phantomjs']
-    },
-
-
     /**
      * chrome
      */
-    {
-      browserName: 'chrome', platform: 'Windows XP',
-      tags: ['ways-addressbar', 'chrome', 'winxp'], 'record-video': true
-    },
-    {
-      browserName: 'chrome', platform: 'Windows 7',
-      tags: ['ways-addressbar', 'chrome', 'win7'], 'record-video': true
-    },
-    {
-      browserName: 'chrome', platform: 'Windows 8',
-      tags: ['ways-addressbar', 'chrome', 'win8'], 'record-video': true
-    },
-    {
-      browserName: 'chrome', platform: 'Linux',
-      tags: ['ways-addressbar', 'chrome', 'linux'], 'record-video': true
-    },
-    {
-      browserName: 'chrome', platform: 'OS X 10.6',
-      tags: ['ways-addressbar', 'chrome', 'snow'], 'record-video': true
-    },
-    {
-      browserName: 'chrome', platform: 'OS X 10.8',
-      tags: ['ways-addressbar', 'chrome', 'mountain'], 'record-video': true
-    },
+    env('OS X 10.6'   , 'chrome'),
+    env('OS X 10.8'   , 'chrome'),
+    env('OS X 10.9'   , 'chrome'),
+    env('OS X 10.10'  , 'chrome'),
+    env('Windows XP'  , 'chrome'),
+    env('Windows 7'   , 'chrome'),
+    env('Windows 8'   , 'chrome'),
+    env('Windows 8.1' , 'chrome'),
+    env('Linux'       , 'chrome'),
 
     /**
      * safari
      */
-    {
-      version: '5',
-      browserName: 'safari', platform: 'Windows 7',
-      tags: ['ways-addressbar', 'safari5', 'win7'], 'record-video': true
-    },
-    {
-      version: '5',
-      browserName: 'safari', platform: 'OS X 10.6',
-      tags: ['ways-addressbar', 'safari5', 'snow'], 'record-video': true
-    },
-    {
-      version: '6',
-      browserName: 'safari', platform: 'OS X 10.8',
-      tags: ['ways-addressbar', 'safari6', 'mountain'], 'record-video': true
-    },
-
+    env('Windows 7'  , 'safari'),
+    env('OS X 10.6'  , 'safari'),
+    env('OS X 10.8'  , 'safari'),
+    env('OS X 10.9'  , 'safari'),
+    env('OS X 10.10' , 'safari'),
+    env('OS X 10.10' , 'safari'),
 
     /**
      * opera
      */
-    {
-      browserName: 'opera', version: '12', platform: 'Linux', 
-      tags: ['ways-addressbar', 'opera', 'linux'], 'record-video': true
-    },
-    {
-      browserName: 'opera', version: '12', platform: 'Windows 7',
-      tags: ['ways-addressbar', 'opera', 'win7'], 'record-video': true
-    },
+    env('Linux'      , 'opera'),
+    env('Windows XP' , 'opera'),
+    env('Windows 7'  , 'opera'),
 
     /**
      * firefox
-     */
-    {
-      browserName: 'firefox', version: '22', platform: 'Windows XP',
-      tags: ['ways-addressbar', 'firefox', 'winxp'], 'record-video': true
-    },
-    {
-      browserName: 'firefox', version: '22', platform: 'Windows 7',
-      tags: ['ways-addressbar', 'firefox', 'win7'], 'record-video': true
-    },
-    {
-      browserName: 'firefox', version: '22', platform: 'Windows 8',
-      tags: ['ways-addressbar', 'firefox', 'win8'], 'record-video': true
-    },
-    {
-      browserName: 'firefox', platform: 'Linux',
-      tags: ['ways-addressbar', 'firefox', 'linux'], 'record-video': true
-    },
-    {
-      browserName: 'firefox', version: '21', platform: 'OS X 10.6',
-      tags: ['ways-addressbar', 'firefox', 'snow'], 'record-video': true
-    },
-    {
-      browserName: 'firefox', version: '22', platform: 'OS X 10.8',
-      tags: ['ways-addressbar', 'firefox', 'snow'], 'record-video': true
-    },
+     */        
+    env('Windows XP'  , 'firefox'),
+    env('Windows 7'   , 'firefox'),
+    env('Windows 8'   , 'firefox'),
+    env('Windows 8.1' , 'firefox'),
+    env('OS X 10.6'   , 'firefox'),
+    env('OS X 10.9'   , 'firefox'),
+    env('OS X 10.10'  , 'firefox'),
+    env('Linux'       , 'firefox'),
 
     /**
      * internet explorer
      */
-    {
-      browserName: 'internet explorer ', version: '6', platform: 'Windows XP',
-      tags: ['ways-addressbar', 'ie6', 'winxp'], 'record-video': true
-    },
-    {
-      browserName: 'internet explorer', version: '7', platform: 'Windows XP',
-      tags: ['ways-addressbar', 'ie7', 'winxp'], 'record-video': true
-    },
-    {
-      browserName: 'internet explorer', version: '8', platform: 'Windows XP',
-      tags: ['ways-addressbar', 'ie8', 'winxp'], 'record-video': true
-    },
-    {
-      browserName: 'internet explorer', version: '8', platform: 'Windows 7',
-      tags: ['ways-addressbar', 'ie8', 'win7'], 'record-video': true
-    },
-    {
-      browserName: 'internet explorer', version: '9', platform: 'Windows 7',
-      tags: ['ways-addressbar', 'ie9', 'win7'], 'record-video': true
-    },
-    {
-      browserName: 'internet explorer', version: '10', platform: 'Windows 8',
-      tags: ['ways-addressbar', 'ie10', 'win8'], 'record-video': true
-    },
-    {
-      browserName: 'internet explorer', version: '11', platform: 'Windows 8.1',
-      tags: ['ways-addressbar', 'ie11', 'win8.1'], 'record-video': true
-    },
+    env('Windows XP'  , 'internet explorer' , 7),
+    env('Windows XP'  , 'internet explorer' , 8),
+    env('Windows XP'  , 'internet explorer' , 9),
+    env('Windows 7'   , 'internet explorer' , 8),
+    env('Windows 7'   , 'internet explorer' , 9),
+    env('Windows 7'   , 'internet explorer' , 10),
+    env('Windows 7'   , 'internet explorer' , 11),
+    env('Windows 8'   , 'internet explorer' , 10),
+    env('Windows 8.1' , 'internet explorer' , 11),
 
     /**
      * ios / safari
      */
-    {
-      browserName: 'iphone', platform: 'OS X 10.8',
-      tags: ['ways-addressbar', 'ios', '6.1', 'iphone'], 'record-video': true
-    },
-    {
-      browserName: 'ipad', platform: 'OS X 10.8',
-      tags: ['ways-addressbar', 'ios', '6.1', 'ipad'], 'record-video': true
-    },
+    env('OS X 10.10' , 'ipad', 6.0, 'iPad Simulator'),
+    env('OS X 10.10' , 'ipad', 6.1, 'iPad Simulator'),
+    env('OS X 10.10' , 'ipad', 7.0, 'iPad Simulator'),
+    env('OS X 10.10' , 'ipad', 7.1, 'iPad Simulator'),
+    env('OS X 10.10' , 'ipad', 8.1, 'iPad Simulator'),
+    env('OS X 10.10' , 'ipad', 8.2, 'iPad Simulator'),
+    env('OS X 10.10' , 'ipad', 8.3, 'iPad Simulator'),
+    env('OS X 10.10' , 'iphone', 6.0, 'iPhone Simulator'),
+    env('OS X 10.10' , 'iphone', 6.1, 'iPhone Simulator'),
+    env('OS X 10.10' , 'iphone', 7.0, 'iPhone Simulator'),
+    env('OS X 10.10' , 'iphone', 7.1, 'iPhone Simulator'),
+    env('OS X 10.10' , 'iphone', 8.1, 'iPhone Simulator'),
+    env('OS X 10.10' , 'iphone', 8.2, 'iPhone Simulator'),
+    env('OS X 10.10' , 'iphone', 8.3, 'iPhone Simulator'),
 
     /**
      * android / browser
      */
-    {
-      browserName: 'android', version: '4.0', platform: 'Linux',
-      tags: ['ways-addressbar', 'android', '4.0', 'phone'], 'record-video': true
-    },
-    {
-      browserName: 'android', version: '4.0',
-      'device-type': 'tablet', platform: 'Linux',
-      tags: ['ways-addressbar', 'android', '4.0', 'tablet'], 'record-video': true
-    }
-
-    /**
-     * android / chrome
-     */
+    env('Linux' , 'android', 4.2, 'LG Nexus 4 Emulator'),
+    env('Linux' , 'android', 4.3, 'LG Nexus 4 Emulator'),
+    env('Linux' , 'android', 4.4, 'LG Nexus 4 Emulator'),
+    env('Linux' , 'android', 4.2, 'Samsung Galaxy Tab 3 Emulator'),
   ];
 };
+
+function env(platform, browser, version, deviceName){
+  var args = Array.prototype.slice.call(arguments, 0);
+  return {
+    platform: platform,
+    browserName: browser,
+    version: version,
+    deviceName: deviceName,
+    name: (platform == null ? 'local' : '') + args.join(' - '),
+    tags: ['ways-addressbar'],
+    'record-video': false
+  };
+}
