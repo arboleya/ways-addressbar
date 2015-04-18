8067bb0a6a48a6bd5b2fe81107da1c7be913e4ee
Anderson Arboleya <anderson@arboleya.me>
2015-04-18 02:22:50 -0300

    drastically reducing code

--- test/tests/utils/browsers.js
+++ test/tests/utils/browsers.js
@@ -15,86 +15,54 @@ exports['local'] = function() {
  */
 exports['sauce labs'] = function() {
   return [
-    /**
-     * chrome
-     */
-    env('OS X 10.6'   , 'chrome'),
-    env('OS X 10.8'   , 'chrome'),
-    env('OS X 10.9'   , 'chrome'),
-    env('OS X 10.10'  , 'chrome'),
-    env('Windows XP'  , 'chrome'),
-    env('Windows 7'   , 'chrome'),
-    env('Windows 8'   , 'chrome'),
-    env('Windows 8.1' , 'chrome'),
-    env('Linux'       , 'chrome'),
 
-    /**
-     * safari
-     */
-    env('Windows 7'  , 'safari'),
-    env('OS X 10.6'  , 'safari'),
-    env('OS X 10.8'  , 'safari'),
-    env('OS X 10.9'  , 'safari'),
-    env('OS X 10.10' , 'safari'),
-    env('OS X 10.10' , 'safari'),
+    // chrome
+    env('Linux', 'chrome', '39'),
+    env('Linux', 'chrome', '40'),
+    env('Linux', 'chrome', '41'),
+    env('Linux', 'chrome', 'beta'),
+    env('Linux', 'chrome', 'dev'),
 
-    /**
-     * opera
-     */
-    env('Linux'      , 'opera'),
-    env('Windows XP' , 'opera'),
-    env('Windows 7'  , 'opera'),
+    // safari
+    env('OS X 10.6', 'safari'),
+    env('OS X 10.7', 'safari'),
+    env('OS X 10.9', 'safari'),
+    env('OS X 10.10', 'safari'),
 
-    /**
-     * firefox
-     */        
-    env('Windows XP'  , 'firefox'),
-    env('Windows 7'   , 'firefox'),
-    env('Windows 8'   , 'firefox'),
-    env('Windows 8.1' , 'firefox'),
-    env('OS X 10.6'   , 'firefox'),
-    env('OS X 10.9'   , 'firefox'),
-    env('OS X 10.10'  , 'firefox'),
-    env('Linux'       , 'firefox'),
+    // firefox
+    env('Linux', 'firefox', '35'),
+    env('Linux', 'firefox', '36'),
+    env('Linux', 'firefox', '37'),
+    env('Linux', 'firefox', 'beta'),
+    env('Linux', 'firefox', 'dev'),
 
-    /**
-     * internet explorer
-     */
-    env('Windows XP'  , 'internet explorer' , '7'),
-    env('Windows XP'  , 'internet explorer' , '8'),
-    // env('Windows XP'  , 'internet explorer' , 9),
-    env('Windows 7'   , 'internet explorer' , '8'),
-    env('Windows 7'   , 'internet explorer' , '9'),
-    env('Windows 7'   , 'internet explorer' , '10'),
-    env('Windows 7'   , 'internet explorer' , '11'),
-    env('Windows 8'   , 'internet explorer' , '10'),
-    env('Windows 8.1' , 'internet explorer' , '11'),
+    // internet explorer
+    env('Windows XP'  , 'internet explorer' , '7')  ,
+    env('Windows XP'  , 'internet explorer' , '8')  ,
+    env('Windows 7'   , 'internet explorer' , '9')  ,
+    env('Windows 8'   , 'internet explorer' , '10') ,
+    env('Windows 8.1' , 'internet explorer' , '11') ,
 
-    /**
-     * ios / safari
-     */
-    env('OS X 10.10' , 'ipad'   , '6.0' , 'iPad Simulator'),
-    env('OS X 10.10' , 'ipad'   , '6.1' , 'iPad Simulator'),
-    env('OS X 10.10' , 'ipad'   , '7.0' , 'iPad Simulator'),
-    env('OS X 10.10' , 'ipad'   , '7.1' , 'iPad Simulator'),
-    env('OS X 10.10' , 'ipad'   , '8.1' , 'iPad Simulator'),
-    env('OS X 10.10' , 'ipad'   , '8.2' , 'iPad Simulator'),
-    env('OS X 10.10' , 'ipad'   , '8.3' , 'iPad Simulator'),
-    env('OS X 10.10' , 'iphone' , '6.0' , 'iPhone Simulator'),
-    env('OS X 10.10' , 'iphone' , '6.1' , 'iPhone Simulator'),
-    env('OS X 10.10' , 'iphone' , '7.0' , 'iPhone Simulator'),
-    env('OS X 10.10' , 'iphone' , '7.1' , 'iPhone Simulator'),
-    env('OS X 10.10' , 'iphone' , '8.1' , 'iPhone Simulator'),
-    env('OS X 10.10' , 'iphone' , '8.2' , 'iPhone Simulator'),
-    env('OS X 10.10' , 'iphone' , '8.3' , 'iPhone Simulator'),
+    // ios / ipad
+    // env('OS X 10.10'    , 'ipad' , '7.0' , 'iPad Simulator') , // wont work!
+    // env('OS X 10.10'    , 'ipad' , '7.1' , 'iPad Simulator') , // wont work!
+    env('OS X 10.10'    , 'ipad' , '8.0' , 'iPad Simulator'),
+    env('OS X 10.10'    , 'ipad' , '8.1' , 'iPad Simulator'),
+    env('OS X 10.10'    , 'ipad' , '8.2' , 'iPad Simulator'),
+    env('OS X 10.10'    , 'ipad' , '8.3' , 'iPad Simulator'),
+  
+    // ios / iphone
+    env('OS X 10.10'    , 'iphone' , '7.0' , 'iPhone Simulator'),
+    env('OS X 10.10'    , 'iphone' , '7.1' , 'iPhone Simulator'),
+    env('OS X 10.10'    , 'iphone' , '8.1' , 'iPhone Simulator'),
+    env('OS X 10.10'    , 'iphone' , '8.2' , 'iPhone Simulator'),
+    env('OS X 10.10'    , 'iphone' , '8.3' , 'iPhone Simulator'),
 
-    /**
-     * android / browser
-     */
-    env('Linux' , 'android', '4.2', 'LG Nexus 4 Emulator'),
-    env('Linux' , 'android', '4.3', 'LG Nexus 4 Emulator'),
-    env('Linux' , 'android', '4.4', 'LG Nexus 4 Emulator'),
-    env('Linux' , 'android', '4.2', 'Samsung Galaxy Tab 3 Emulator'),
+    // android / browser
+    env('Linux' , 'android' , '4.2' , 'LG Nexus 4 Emulator')           ,
+    env('Linux' , 'android' , '4.3' , 'LG Nexus 4 Emulator')           ,
+    env('Linux' , 'android' , '4.4' , 'LG Nexus 4 Emulator')           ,
+    env('Linux' , 'android' , '4.2' , 'Samsung Galaxy Tab 3 Emulator'),
   ];
 };
 
@@ -107,6 +75,6 @@ function env(platform, browser, version, deviceName){
     deviceName: deviceName,
     name: (platform == null ? 'local' : '') + args.join(' - '),
     tags: ['ways-addressbar'],
-    'record-video': false
+    'record-video': true
   };
 }
